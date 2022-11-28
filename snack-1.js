0b9a73504e8b52190513fe7305e448ce2cd02e7f
Paolo224 <picariellopaolo224@gmail.com>
2022-11-28 16:32:02 +0100

    Primo Snack completo

---
+++ snack-1.js
@@ -0,0 +1,10 @@
+
+
+let numero = parseInt(prompt('Inserisci un numero da 1 a 10'));
+
+while((numero < 1) || (numero >= 10)){
+    numero = parseInt(prompt('Inserisci un numero da 1 a 10'));
+}
+
+console.log(numero)
+
